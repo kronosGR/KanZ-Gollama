fa0b7d067abfc9bd617650401c6ac452addd6127
kronosGR <geo.elgeo@gmail.com>
2025-01-29 14:01:27 +0100

    added partial functionality on delete button

--- src/renderer/src/components/ModelListItem.tsx
+++ src/renderer/src/components/ModelListItem.tsx
@@ -9,15 +9,29 @@ interface IProps {
 }
 
 export const ModelListItem: React.FC<IProps> = ({ model }) => {
+  const deleteModel = async (e: React.MouseEvent<SVGAElement>): Promise<void> => {
+    e.preventDefault()
+    console.log(e.currentTarget.id)
+  }
   return (
-    <div className="flex w-full hover:bg-gray-100  ">
-      <div className="w-2/6">{model.name}</div>
-      <div className="w-1/6">{model.family}</div>
-      <div className="w-1/6">{model.format}</div>
-      <div className="w-1/12">{model.parameterSize}</div>
-      <div className="w-1/12">{convertBytes(model.size)}</div>
-      <div className="w-1/12 flex justify-end items-center text-red-700">
-        <MdDeleteForever />
+    <div className="flex w-full hover:bg-gray-100 text-sm cursor-text  ">
+      <div className="w-2/6 ms-3" title="Model Name">
+        {model.name}
+      </div>
+      <div className="w-1/6" title="Model Family">
+        {model.family}
+      </div>
+      <div className="w-1/6" title="Model Format">
+        {model.format}
+      </div>
+      <div className="w-1/6 flex justify-end items-end" title="Model Parameter Size">
+        {model.parameterSize}
+      </div>
+      <div className="w-1/6 flex justify-end items-end" title="Model Size">
+        {convertBytes(model.size)}
+      </div>
+      <div className="w-1/12 flex justify-end items-center text-red-700" title="Remove Model">
+        <MdDeleteForever id={model.name} className="cursor-pointer" onClick={deleteModel} />
       </div>
     </div>
   )
