7e1b274b9fc662ebde43a3cb075c72bcb104aaa8
kronosGR <geo.elgeo@gmail.com>
2025-02-23 12:33:33 +0100

    changed to Markdown plugin

--- src/renderer/src/components/ChatItem.tsx
+++ src/renderer/src/components/ChatItem.tsx
@@ -1,7 +1,8 @@
 import { IMessage } from '@renderer/interfaces/IMessage'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HiMiniUserGroup } from 'react-icons/hi2'
 import { FaBrain } from 'react-icons/fa'
+import Markdown from 'react-markdown'
 import showdown from 'showdown'
 
 interface IProps {
@@ -13,19 +14,22 @@ export const ChatItem: React.FC<IProps> = ({ message }) => {
   const bgColor = message?.role === 'assistant' ? ' bg-blue-200' : 'j bg-green-200'
   const color = message?.role === 'assistant' ? ' text-blue-400' : 'j text-green-400'
 
-  const converter = new showdown.Converter()
-  const chat = message?.content
-
   return (
     <div className="flex w-[100%] mb-2">
       <div className={`flex w-[100%] items-center ${location}`}>
         <div>
           {message?.role === 'assistant' && <FaBrain className={`text-4xl mr-1 ${color}`} />}
         </div>
-        <div
+        {
+          <div className={`w-[80%] border p-2 text-xs  overflow-auto ${bgColor}`}>
+            <Markdown>{message?.content}</Markdown>
+          </div>
+        }
+        {/* <div
           className={`w-[80%] border p-2 text-xs  overflow-auto ${bgColor}`}
           dangerouslySetInnerHTML={{ __html: converter.makeHtml(chat) }}
-        ></div>
+        ></div> */}
+
         {message?.role === 'user' && <HiMiniUserGroup className={`text-4xl mr-1 ${color}`} />}
       </div>
     </div>
